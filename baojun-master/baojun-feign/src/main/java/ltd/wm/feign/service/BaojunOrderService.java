c7ffcb477a047117d2e8a04b6df0d39812323b0c
tanxin <tanxin_java@163.com>
2018-03-16 11:52:53 +0800

    mer

--- baojun-master/baojun-feign/src/main/java/ltd/wm/feign/service/BaojunOrderService.java
+++ baojun-master/baojun-feign/src/main/java/ltd/wm/feign/service/BaojunOrderService.java
@@ -4,14 +4,9 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-<<<<<<< HEAD
 import ltd.wm.feign.hystric.FallbackOrderHystric;
 
 @FeignClient(value = "order-model",fallback = FallbackOrderHystric.class)
-=======
-@FeignClient(value = "order-model")
->>>>>>> 3761f75c1404cbc3f63072329e25c20ae3dbad8b
 public interface BaojunOrderService {
 
 	@RequestMapping(value = "/hi", method = RequestMethod.GET)
